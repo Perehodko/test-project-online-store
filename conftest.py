aaeaad2a3a3100761fffd5bc6f837d0c92f9695d
nper <nper@n_perehodko.mtespb.lan>
2019-07-30 10:37:10 +0300

    add test go_to_login_page

---
+++ conftest.py
@@ -0,0 +1,22 @@
+import pytest
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default="en", help="Choose language")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    language = request.config.getoption("language")
+    browser = webdriver.Chrome()
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    browser = webdriver.Chrome(options=options)
+    
+    yield browser
+    print("\nquit browser..")
+    time.sleep(1)
+    #browser.quit()
+
